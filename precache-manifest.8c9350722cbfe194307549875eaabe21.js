5c3e04def266609ec81aeb23acc9cae0b30a9da0
Michael Vargas <msvargas97@gmail.com>
2019-10-07 10:11:32 -0500

    Updates

---
+++ precache-manifest.8c9350722cbfe194307549875eaabe21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cfb1b32579509acc3c07c01dafbf9de",
+    "url": "/pico.js/examples/webcam/index.html"
+  },
+  {
+    "revision": "d9951bcb09d4170a5cc4",
+    "url": "/pico.js/examples/webcam/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "71ef07b241d096d2f5d7",
+    "url": "/pico.js/examples/webcam/static/js/2.770f4e9c.chunk.js"
+  },
+  {
+    "revision": "d9951bcb09d4170a5cc4",
+    "url": "/pico.js/examples/webcam/static/js/main.e5604786.chunk.js"
+  },
+  {
+    "revision": "5ecb3669bb80a92e1fbc",
+    "url": "/pico.js/examples/webcam/static/js/runtime-main.b1038629.js"
+  }
+]);
