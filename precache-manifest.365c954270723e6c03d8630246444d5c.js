6fbee54aa0a3762b760fd7d83947c8c514d5bfee
Michael Vargas <msvargas97@gmail.com>
2019-10-07 10:14:38 -0500

    Updates

---
+++ precache-manifest.365c954270723e6c03d8630246444d5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e90cbc32431dd7812f5b0bd00eae3323",
+    "url": "/pico.js/demo/index.html"
+  },
+  {
+    "revision": "4fab6f7bb0ee4505ec14",
+    "url": "/pico.js/demo/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "71ef07b241d096d2f5d7",
+    "url": "/pico.js/demo/static/js/2.770f4e9c.chunk.js"
+  },
+  {
+    "revision": "4fab6f7bb0ee4505ec14",
+    "url": "/pico.js/demo/static/js/main.680afe28.chunk.js"
+  },
+  {
+    "revision": "5be0579e40cde834a75a",
+    "url": "/pico.js/demo/static/js/runtime-main.4cf0c761.js"
+  }
+]);
